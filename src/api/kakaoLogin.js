d88bef28f063eaffc47795b9d2c9579bae54a71a
choi <wjfekr1@naver.com>
2023-03-16 20:49:25 +0900

    dasfsga

--- src/api/kakaoLogin.js
+++ src/api/kakaoLogin.js
@@ -1,6 +1,14 @@
+import axios from "axios";
+
 const kakaoLoginApi = () => {
-  const REDIRECT_URI = "https://sosohandiary.shop/oauth";
-  window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const REDIRECT_URI = "https://sosohandiary.shop/login/kakao";
+  console.log("ddd");
+  axios
+    .get(
+      `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+    )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 };
 
 export { kakaoLoginApi };
