97ef452e40ac4d3e20230af07613788a88933d68
choi <wjfekr1@naver.com>
2023-03-17 23:49:05 +0900

    회원 가입부터 다이어리 리스트 UI 프레임 작업

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Friendlist from "../components/Friendlist";
 import Navigationbar from "../components/Navigationbar";
 import Searchbox from "../components/Searchbox";
--- src/pages/SignupSuccess.jsx
+++ src/pages/SignupSuccess.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import { WholeAreaStyle } from "../styles/\bWholeAreaStyle";
+import { LongButtonStyle } from "../styles/LongButtonStyle";
 
 const SignupSuccess = () => {
-  return <div style={{ margin: "10vw" }}>SignupSuccess</div>;
+  return (
+    <WholeAreaStyle>
+      <CelebratingMsg>
+        소다님,
+        <br />
+        회원가입을 축하합니다!
+      </CelebratingMsg>
+      <Circle></Circle>
+      <WelcomeMsg>이제부터 소소한 일상을 담아보세요!</WelcomeMsg>
+      <LongButtonStyle>다이어리 쓰러 가기</LongButtonStyle>
+    </WholeAreaStyle>
+  );
 };
 
 export default SignupSuccess;
+
+const CelebratingMsg = styled.h2`
+  margin-top: 8vh;
+`;
+const Circle = styled.div`
+  background-color: #d6d6d6;
+  height: 30vh;
+  width: 30vh;
+  border-radius: 50%;
+  margin: 8vh 0px;
+`;
+
+const WelcomeMsg = styled.div`
+  margin-top: 2vh;
+  margin-bottom: 8vh;
+`;
