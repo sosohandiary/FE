7656ae1457522a74d8e7bf08408c5660cf5599b6
Choi Seungho <107627925+boompeak@users.noreply.github.com>
2023-03-18 17:35:13 +0900

    Add files via upload

---
+++ src/styles/_WholeAreaStyle.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const WholeAreaWithMargin = styled.div`
+  margin: 30px 8vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const WholeArea = styled.div`
+  margin: 30px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
