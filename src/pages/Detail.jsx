b17f2b58b8c04bc006aa80083fe3770e43597b01
leejuae1020 <leejuae1020@naver.com>
2023-03-18 15:59:45 +0900

    댓글구현 컴포넌트 및 파일 삭제

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import AddcommentForm from "../components/detail/AddcommentForm";
-import CommentList from "../components/detail/CommentList";
 
 const Detail = () => {
-  return (
-    <div>
-      <AddcommentForm />
-      <CommentList />
-    </div>
-  );
+  return <div>상세ㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔㅔ</div>;
 };
 
 export default Detail;
