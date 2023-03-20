0bd252402d8ee114e468a45eb39306bba3e250a7
leejuae1020 <leejuae1020@naver.com>
2023-03-20 11:10:56 +0900

    충돌수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Carousel from "../components/Carousel";
-import {
-  WholeAreaWithMargin,
-  WholeViewWidth,
-} from "../styles/\bWholeAreaStyle";
+import { WholeAreaWithMargin, WholeViewWidth } from "../styles/WholeAreaStyle";
 import { MintButtonLarge } from "../styles/Buttons";
 import { LongButtonStyle } from "../styles/LongButtonStyle";
 const MainPage = () => {
@@ -17,9 +14,7 @@ const MainPage = () => {
       <WholeViewWidth>
         <Carousel />
       </WholeViewWidth>
-      <MintButtonLarge onClick={goToLogin}>
-        공유 다이어리 시작하기
-      </MintButtonLarge>
+      <MintButtonLarge onClick={goToLogin}>공유 다이어리 시작하기</MintButtonLarge>
     </WholeAreaWithMargin>
   );
 };
