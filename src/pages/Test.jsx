9dfdf310f5fc4aa5ac04d76016fa20e42fe692ab
choi <wjfekr1@naver.com>
2023-03-23 10:23:40 +0900

    꾸미기 모드 구현 완료

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -3,9 +3,164 @@ import { useEffect, useRef, useState } from "react";
 import React from "react";
 import styled from "styled-components";
 import { WholeAreaWithMargin } from "../styles/WholeAreaStyle";
-import { Stage, Layer, Star, Text, Line, Rect, Transformer } from "react-konva";
+import {
+  Stage,
+  Layer,
+  Star,
+  Text,
+  Line,
+  Rect,
+  Transformer,
+  Image,
+} from "react-konva";
 import TestDraft from "./TestDraft";
-import Draggable from "react-draggable";
+import useImage from "use-image";
+
+// <---------------변형 기능된 이미지 스티커 컴퍼넌트----------------->
+const ImageSticker = ({
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+  sticker,
+  mode,
+}) => {
+  const shapeRef = React.useRef();
+  const trRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  // 스티커 사전
+  const [imgUrl0] = useImage("https://konvajs.org/assets/lion.png");
+  const [imgUrl1] = useImage("https://konvajs.org/assets/yoda.jpg");
+  const [imgUrl2] = useImage(
+    "https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"
+  );
+
+  const imgList = [imgUrl0, imgUrl1, imgUrl2];
+
+  return (
+    <React.Fragment>
+      <Image
+        image={imgList[sticker.stickerUrlNum]}
+        key={sticker.id}
+        id={sticker.id}
+        x={sticker.x}
+        y={sticker.y}
+        numPoints={5}
+        innerRadius={20}
+        outerRadius={40}
+        // fill="#89b717"
+        opacity={0.8}
+        draggable={mode === "STICKER" ? true : false}
+        rotation={sticker.rotation}
+        shadowColor="black"
+        shadowBlur={10}
+        shadowOpacity={0.6}
+        shadowOffsetX={sticker.isDragging ? 10 : 5}
+        shadowOffsetY={sticker.isDragging ? 10 : 5}
+        scaleX={sticker.isDragging ? 1.2 : 1}
+        scaleY={sticker.isDragging ? 1.2 : 1}
+        onDragStart={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+            isDragging: true,
+          });
+        }}
+        // onDragEnd={handleDragEnd}
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+            isDragging: false,
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </React.Fragment>
+  );
+};
+
+// <---------------------------------------->
+
+// 초기 스티커 값
+const INITIAL_STATE = [
+  {
+    id: 0,
+    x: 100,
+    y: 100,
+    width: 100,
+    height: 100,
+    rotation: 30,
+    isDragging: false,
+    stickerUrlNum: 0,
+  },
+  {
+    id: 1,
+    x: 200,
+    y: 200,
+    width: 100,
+    height: 100,
+    rotation: 60,
+    isDragging: false,
+    stickerUrlNum: 1,
+  },
+  {
+    id: 2,
+    x: 300,
+    y: 300,
+    width: 100,
+    height: 100,
+    rotation: 90,
+    isDragging: false,
+    stickerUrlNum: 2,
+  },
+];
 
 const Test = () => {
   const [mode, setMode] = useState("TEXT");
@@ -22,30 +177,35 @@ const Test = () => {
 
   const [stickers, setStickers] = useState([]);
 
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setStickers(
-      stickers.map((sticker) => {
-        return {
-          ...sticker,
-          isDragging: sticker.id === id,
-        };
-      })
-    );
-  };
-  const handleDragEnd = (e) => {
-    setStickers(
-      stickers.map((sticker) => {
-        return {
-          ...sticker,
-          isDragging: false,
-        };
-      })
-    );
+  // 변형할 것 선택 관련
+  const [selectedId, selectShape] = React.useState(null);
+
+  // 스티커 추가 관련
+  const addStickerButtonHandler = (num) => {
+    const newSticker = {
+      id: stickers.length,
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      width: 100,
+      height: 100,
+      rotation: 45,
+      isDragging: false,
+      stickerUrlNum: num,
+    };
+    const newStickerList = stickers.concat();
+    newStickerList.push(newSticker);
+    setStickers(newStickerList);
   };
 
   // <-------------- 그리기 관련 -------------->
   const handleMouseDown = (e) => {
+    //스티커 변형 초기화
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+
+    //그리기 시작
     if (mode === "DRAW") {
       isDrawing.current = true;
       const pos = e.target.getStage().getPointerPosition();
@@ -86,8 +246,6 @@ const Test = () => {
     setLineTool(target);
   };
 
-  const addStickerHandler = () => {};
-
   //도구 모음 창
   const Toolbar = () => {
     return (
@@ -105,17 +263,15 @@ const Test = () => {
         <button onClick={() => changeLineTool("pen")}>펜</button>
         <div>
           스티커 관련
-          <div>
-            별
-            <Draggable>
-              <button onClick={() => addStickerHandler()}>큰 별 추가</button>
-            </Draggable>
-            <button onClick={() => addStickerHandler()}>작은 별 추가</button>
-          </div>
-          <div>
-            원<button onClick={() => addStickerHandler()}>큰 원 추가</button>
-            <button onClick={() => addStickerHandler()}>작은 원 추가</button>
-          </div>
+          <button onClick={() => addStickerButtonHandler(0)}>
+            0번 스티커 추가
+          </button>
+          <button onClick={() => addStickerButtonHandler(1)}>
+            1번 스티커 추가
+          </button>
+          <button onClick={() => addStickerButtonHandler(2)}>
+            2번 스티커 추가
+          </button>
         </div>
       </div>
     );
@@ -133,6 +289,7 @@ const Test = () => {
         width={window.innerWidth}
         height={window.innerHeight}
         onMouseDown={handleMouseDown}
+        onTouchStart={handleMouseDown}
         onMousemove={handleMouseMove}
         onMouseup={handleMouseUp}
         style={{ position: "absolute" }}
@@ -152,29 +309,24 @@ const Test = () => {
               }
             />
           ))}
-          {stickers.map((sticker) => {
+          {stickers.map((sticker, i) => {
             return (
-              <Star
-                key={sticker.id}
-                id={sticker.id}
-                x={sticker.x}
-                y={sticker.y}
-                numPoints={5}
-                innerRadius={20}
-                outerRadius={40}
-                fill="#89b717"
-                opacity={0.8}
-                draggable={mode === "STICKER" ? true : false}
-                rotation={sticker.rotation}
-                shadowColor="black"
-                shadowBlur={10}
-                shadowOpacity={0.6}
-                shadowOffsetX={sticker.isDragging ? 10 : 5}
-                shadowOffsetY={sticker.isDragging ? 10 : 5}
-                scaleX={sticker.isDragging ? 1.2 : 1}
-                scaleY={sticker.isDragging ? 1.2 : 1}
-                onDragStart={handleDragStart}
-                onDragEnd={handleDragEnd}
+              <ImageSticker
+                key={i}
+                mode={mode}
+                shapeProps={sticker}
+                isSelected={sticker.id === selectedId}
+                sticker={sticker}
+                onSelect={() => {
+                  if (mode === "STICKER") {
+                    selectShape(sticker.id);
+                  }
+                }}
+                onChange={(newAttrs) => {
+                  const sticks = stickers.slice();
+                  sticks[i] = newAttrs;
+                  setStickers(sticks);
+                }}
               />
             );
           })}
