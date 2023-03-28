7b1122daf88a74b4e5a5a6f6d1ae7249f18594eb
seryoungk <dmckuromi@gmail.com>
2023-03-28 18:52:55 +0900

    라우터에 Diary 추가

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -21,7 +21,7 @@ import TestCrop from "./TestCrop";
 import DiaryDetail from "./DiaryDetail";
 import TestAnimation from "./TestAnimation";
 import AcceptTest from "./AcceptTest";
-
+import Diary from "./Diary";
 const Router = () => {
   return (
     <BrowserRouter>
@@ -44,6 +44,7 @@ const Router = () => {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/new-diary" element={<NewDiary />} />
+        <Route path="/diary" element={<Diary />} />
         <Route path="/diary-detail" element={<DiaryDetail />} />
         <Route path="/myfriends/:mode" element={<MyFriends />} />
         <Route path="/acceptTest" element={<AcceptTest />} />
