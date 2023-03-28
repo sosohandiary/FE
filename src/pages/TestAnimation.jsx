0147f3156c7a73a9704bd6f4d2fc98f4e65f02ca
choi <wjfekr1@naver.com>
2023-03-28 15:49:39 +0900

    메인 페이지 공유/공개는 무한 스크롤 구현함

--- src/pages/TestAnimation.jsx
+++ src/pages/TestAnimation.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade } from "swiper";
 
+// Import Swiper styles
 import "swiper/css";
-import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-const TestAnimation = () => {
+export default () => {
   return (
-    <Swiper modules={[EffectFade]} effect="fade">
-      {[1, 2, 3].map((i, el) => {
-        return <SwiperSlide>Slide {el}</SwiperSlide>;
-      })}
+    <Swiper
+      // install Swiper modules
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
     </Swiper>
   );
 };
-
-export default TestAnimation;
