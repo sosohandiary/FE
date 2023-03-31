2996a218e8e99cc8cb2bfa96903e74be11a43722
choi <wjfekr1@naver.com>
2023-03-31 14:31:50 +0900

    다이어리 상세보기까지 완료

--- src/pages/TestAnimation.jsx
+++ src/pages/TestAnimation.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import HTMLFlipBook from "react-pageflip";
+import React, { useRef } from "react";
+import html2canvas from "html2canvas";
 
-const TestAnimation = () => {
+function TestAnimation() {
+  const myRef = useRef(null);
+
+  const handleScreenshotClick = () => {
+    html2canvas(myRef.current).then((canvas) => {
+      console.log(canvas);
+      const dataURL = canvas.toDataURL();
+
+      // Set the src attribute of an image tag to the data URL
+      const img = document.createElement("img");
+      img.src = dataURL;
+      // Do something with the canvas, like displaying it in an image tag or sending it to a server
+    });
+  };
+
   return (
-    <HTMLFlipBook width={300} height={500}>
-      <div className="demoPage">Page 1</div>
-      <div className="demoPage">Page 2</div>
-      <div className="demoPage">Page 3</div>
-      <div className="demoPage">Page 4</div>
-    </HTMLFlipBook>
+    <div>
+      <h1>Hello, World!</h1>
+      <div ref={myRef}>
+        <p>This is the content that will be captured in the screenshot</p>
+      </div>
+      <button onClick={handleScreenshotClick}>Take Screenshot</button>
+    </div>
   );
-};
+}
 
 export default TestAnimation;
