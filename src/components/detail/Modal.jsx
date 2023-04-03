3d7b83c1b82ff34972c0409c1fb70a1ebc88700f
leejuae <leejuae1020@naver.com>
2023-04-03 20:08:27 +0900

    수정

--- src/components/detail/Modal.jsx
+++ src/components/detail/Modal.jsx
@@ -1,45 +1,44 @@
 import React from "react";
-import styled from "styled-components";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Box from "@mui/material/Box";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const StyledButton = styled(Button)`
-  color: white;
-  border-radius: 5px;
-  font-size: 1rem;
-  padding: 10px;
-  margin: 5px;
-`;
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f7cad0",
+    },
+    secondary: {
+      main: "#f57777",
+    },
+  },
+});
 
-const StyledModifyButton = styled(StyledButton)`
-  background-color: #f4f4f4;
-`;
-
-const StyledDeleteButton = styled(StyledButton)`
-  background-color: #f4f4f4;
-`;
-
-const StyledCancelButton = styled(StyledButton)`
-  color: #f4f4f4;
-  margin-top: 8px;
-`;
+const buttons = [
+  <Button key="one" color="primary">
+    수정
+  </Button>,
+  <Button key="two" color="secondary">
+    삭제
+  </Button>,
+];
 
 const Modal = () => (
-  <Box
-    sx={{
-      display: "flex",
-      "& > *": {
-        m: 1,
-      },
-    }}
-  >
-    <ButtonGroup orientation="vertical" aria-label="vertical outlined button group">
-      <StyledModifyButton>수정하기</StyledModifyButton>
-      <StyledDeleteButton>삭제하기</StyledDeleteButton>
-      <StyledCancelButton>취소하기</StyledCancelButton>
-    </ButtonGroup>
-  </Box>
+  <ThemeProvider theme={theme}>
+    <Box
+      sx={{
+        display: "flex",
+        "& > *": {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup orientation="vertical" aria-label="vertical contained button group" variant="contained">
+        {buttons}
+      </ButtonGroup>
+    </Box>
+  </ThemeProvider>
 );
 
 export default Modal;
