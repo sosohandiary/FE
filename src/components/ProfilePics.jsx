2c6decb23fbdd5f6d94034668561768a54b51c10
JellyKingdom <jyhan1993@gmail.com>
2023-04-10 16:17:40 +0900

    Style: profile box-shadow

--- src/components/ProfilePics.jsx
+++ src/components/ProfilePics.jsx
@@ -30,7 +30,7 @@ const CirclePic = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); */
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
   width: ${({ size }) => {
     return size === "large" ? "120px" : size === "medium" ? "80px" : "43px";
