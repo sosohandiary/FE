8af6298711034eb2408ac7a53c1d52304366da81
choi <wjfekr1@naver.com>
2023-04-11 15:05:47 +0900

    알람(인바이트) 이동 수정

--- src/pages/UploadSticker.jsx
+++ src/pages/UploadSticker.jsx
@@ -1,6 +1,10 @@
+import axios from "axios";
 import React from "react";
 
 const UploadSticker = () => {
+  const getStickers = () => {
+    axios.get(`${process.env.REACT_APP_BASEURL}/decorations`);
+  };
   return <div>UploadSticker</div>;
 };
 
