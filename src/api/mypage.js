1c63c37a6ec870900d80b70d3c2dfd93df156475
JellyKingdom <jyhan1993@gmail.com>
2023-04-11 15:16:28 +0900

    Fix: try ~ catch in mypage.js

--- src/api/mypage.js
+++ src/api/mypage.js
@@ -1,81 +1,127 @@
 import axios from "axios";
 
 export const getMypage = async (accessToken) => {
-  return await axios.get(`${process.env.REACT_APP_BASEURL}/mypage/diaries`, {
-    headers: { Authorization: accessToken },
-  });
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASEURL}/mypage/diaries`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+    return res;
+  } catch (err) {
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 export const getProfile = async (accessToken) => {
-  return await axios.get(`${process.env.REACT_APP_BASEURL}/mypage/profile`, {
-    headers: { Authorization: accessToken },
-  });
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASEURL}/mypage/profile`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+
+    return res;
+  } catch (err) {
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 export const editProfile = async (formData, accessToken) => {
-  try{
+  try {
     const respense = await axios.patch(
-    `${process.env.REACT_APP_BASEURL}/mypage/profile/edit`,
-    formData,
-    {
-      headers: { Authorization: accessToken },
-      "Content-Type": `multipart/form-data; `,
-    }
-  );
-  return respense.data;
-  }catch(error){
-    console.error(error);
+      `${process.env.REACT_APP_BASEURL}/mypage/profile/edit`,
+      formData,
+      {
+        headers: { Authorization: accessToken },
+        "Content-Type": `multipart/form-data; `,
+      }
+    );
+    return respense.data;
+  } catch (error) {
+    console.error(`Error : ${error} `);
     throw error;
   }
 };
 
 export const getFriendsCount = async (accessToken) => {
-  return await axios.get(
-    `${process.env.REACT_APP_BASEURL}/mypage/friend/count`,
-    {
-      headers: { Authorization: accessToken },
-    }
-  );
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASEURL}/mypage/friend/count`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+    return res;
+  } catch (err) {
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 export const getDiaryCount = async (accessToken) => {
-  return await axios.get(
-    `${process.env.REACT_APP_BASEURL}/mypage/diary/count`,
-    {
-      headers: { Authorization: accessToken },
-    }
-  );
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASEURL}/mypage/diary/count`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+
+    return res;
+  } catch (err) {
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 export const deleteAccount = async (accessToken) => {
-  return await axios.delete(`${process.env.REACT_APP_BASEURL}/mypage/out`, {
-    headers: { Authorization: accessToken },
-  });
+  try {
+    const res = await axios.delete(
+      `${process.env.REACT_APP_BASEURL}/mypage/out`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+    return res;
+  } catch (err) {
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 //친구목록 조회
 export const getMyfriends = async (accessToken) => {
-  return await axios.get(
-    `${process.env.REACT_APP_BASEURL}/mypage/friend/myfriends`,
-    {
-      headers: { Authorization: accessToken },
-    }
-  );
+  try{
+    const res = await axios.get(
+      `${process.env.REACT_APP_BASEURL}/mypage/friend/myfriends`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+    return res;
+  }catch(err){
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
 
 //친구 삭제
 export const deleteFriend = async (friendId, accessToken) => {
-  return await axios.delete(
-    `${process.env.REACT_APP_BASEURL}/friend/list/${friendId}`,
-    {
-      headers: { Authorization: accessToken },
-    }
-  );
-};
-
-//친구추가요청목록
-export const getRequested = async (accessToken) => {
-  return await axios.get(`${process.env.REACT_APP_BASEURL}/friend/request`, {
-    headers: { Authorization: accessToken },
-  });
+  try{
+    const res = await axios.delete(
+      `${process.env.REACT_APP_BASEURL}/friend/list/${friendId}`,
+      {
+        headers: { Authorization: accessToken },
+      }
+    );
+    return res;
+  }catch(err){
+    console.error(`Error : ${err} `);
+    return;
+  }
 };
